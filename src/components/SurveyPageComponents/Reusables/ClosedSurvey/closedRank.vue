25a341ae08d1fd0ce714a3c8e306ce9c15d56b2c
bridgetf7 <fbridget0404@gmail.com>
2024-04-13 13:53:31 -0400

    fixed the way questions shows up on closed ranks

--- src/components/SurveyPageComponents/Reusables/ClosedSurvey/closedRank.vue
+++ src/components/SurveyPageComponents/Reusables/ClosedSurvey/closedRank.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="lg:text-left text-center h-full w-full">
     <div class="flex flex-col lg:flex-row h-full w-full">
-        <div class="flex items-center justify-center max-w-[40rem] overflow-hidden w-full">
+        <div class="flex items-center justify-center overflow-hidden w-full">
           <fieldset class="flex items-center justify-start w-full">
-            <legend class="text-lg xl:leading-10 md:text-xl xl:text-3xl overflow-visible"> Ranked {{ question.question.replace("Select the", " ").replace("course(s) you are interested in:", " ") }} course(s)</legend> 
+            <legend class="text-lg xl:leading-10 md:text-xl xl:text-3xl overflow-visible mb-4"> {{ question.question }} </legend> 
         <div class="block py-2 px-3 mt-3 w-full md:w-3/5 text-base md:text-lg xl:text-2xl bg-transparent rounded-md border border-solid border-zinc-400 focus:outline-none focus:ring-0 focus:border-blue-400">
           <div v-if="(surveyStore.currentResponse[index] as surveyAnswer).answer.preference.length != 0">
             <p v-for="choice in (surveyStore.currentResponse[index] as surveyAnswer).answer.preference" :key="choice.rank" class="mt-2 mb-2 text-base md:text-lg xl:text-2xl">
