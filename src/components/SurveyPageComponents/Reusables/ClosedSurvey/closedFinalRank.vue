dfd4b0515c4f4c81c27f916ece48afd836d0d77b
bridgetf7 <fbridget0404@gmail.com>
2024-04-13 13:12:46 -0400

    fix due date, implement closedSurveys

--- src/components/SurveyPageComponents/Reusables/ClosedSurvey/closedFinalRank.vue
+++ src/components/SurveyPageComponents/Reusables/ClosedSurvey/closedFinalRank.vue
@@ -6,7 +6,7 @@
         <div class="block py-2 px-3 mt-3 w-full md:w-3/5 text-base md:text-lg xl:text-2xl bg-transparent rounded-md border border-solid border-zinc-400 focus:outline-none focus:ring-0 focus:border-blue-400">
           <div v-if="(surveyStore.currentResponse[indexAll] as allCoursesAnswer).answer.preference.length != 0">
             <p v-for="choice in courses" :key="choice.rank" class="mb-2 mt-2 text-base md:text-lg xl:text-2xl">
-            <strong> {{choice.rank}}.) </strong> {{choice.name}}
+            <strong> {{choice.rank + 1}}. </strong> {{choice.name}}
           </p>
           </div>
           <div v-else>
--- src/components/SurveyPageComponents/Reusables/SurveyDropdown.vue
+++ src/components/SurveyPageComponents/Reusables/SurveyDropdown.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="flex items-center justify-center w-full">
-    <fieldset class="flex flex-col justify-start" :aria-invalid="warn" :aria-describedby="warn ? question.id+'required' : ''">
-      <legend class="text-lg xl:leading-10 md:text-xl xl:text-3xl overflow-visible mb-4">{{ question.question }}</legend>
-        <div class="flex flex-row items-baseline">
-      <select
-        class="block py-2 px-3 mt-3 w-full md:w-3/5 text-base md:text-lg xl:text-2xl bg-transparent rounded-md border border-solid focus:outline-none focus:ring-0 disabled:bg-gray-100 "
-        :class="warn ? 'focus:border-red-500 border-red-400 bg-[#FEE2E2]' : 'focus:border-blue-400 border-zinc-400 bg-transparent'"
-        :disabled="isDisabled" v-model="surveyStore.currentResponse[index].answer">
-        <option v-for="value in options" :key="value" :value="value">{{ value }}</option>
-      </select>
-      <Transition enter-from-class="opacity-0" enter-active-class="transition-opacity duration-200 ease-linear"
-        leave-to-class="opacity-0" leave-active-class="transition-opacity duration-200 ease-linear">
-        <div class="flex items-center justify-center text-red-500 gap-2 pb-2 xl:ml-10 md:ml-8 xs:ml-4 ml-2" v-if="warn">
-          <exclamationMark />
-        </div>
-      </Transition>
-        </div>
+    <fieldset class="flex flex-col justify-start" :aria-invalid="warn"
+      :aria-describedby="warn ? question.id + 'required' : ''">
+      <legend class="text-lg xl:leading-10 md:text-xl xl:text-3xl overflow-visible mb-4">{{ question.question }}
+      </legend>
+      <div class="flex flex-row items-baseline">
+        <select
+          class="block py-2 px-3 mt-3 w-full md:w-3/5 text-base md:text-lg xl:text-2xl bg-transparent rounded-md border border-solid focus:outline-none focus:ring-0 disabled:bg-gray-200 "
+          :class="warn ? 'focus:border-red-500 border-red-400 bg-[#FEE2E2]' : 'focus:border-blue-400 border-zinc-400 bg-transparent'"
+          :disabled="isDisabled" v-model="surveyStore.currentResponse[index].answer">
+          <option v-for="value in options" :key="value" :value="value">{{ value }}</option>
+        </select>
+        <Transition enter-from-class="opacity-0" enter-active-class="transition-opacity duration-200 ease-linear"
+          leave-to-class="opacity-0" leave-active-class="transition-opacity duration-200 ease-linear">
+          <div class="flex items-center justify-center text-red-500 gap-2 pb-2 xl:ml-10 md:ml-8 xs:ml-4 ml-2"
+            v-if="warn">
+            <exclamationMark />
+          </div>
+        </Transition>
+      </div>
     </fieldset>
   </div>
 </template>
